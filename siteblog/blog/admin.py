0fb2e37db9810563d279010c8cb2dcde2aac9f76
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2022-03-10 10:01:27 +0300

    main

---
+++ siteblog/blog/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import *
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
+
